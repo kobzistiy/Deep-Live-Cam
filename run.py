e40091fa18b96227f61d540adfd5e0dc412f9532
Aleksey <54094341+kobzistiy@users.noreply.github.com>
2026-01-29 12:27:45 +0500

    Implement dynamic library installation in run.py
    Add dynamic installation of Torch and ONNXRuntime if not found.

--- run.py
+++ run.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 
-# Import the tkinter fix to patch the ScreenChanged error
-import tkinter_fix
+import sys
+import os
+import subprocess
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
+libs_path = os.path.join(base_dir, "libs")
+if not os.path.exists(libs_path):
+    os.makedirs(libs_path)
+sys.path.insert(0, libs_path)
+
+try:
+    import torch
+    import onnxruntime
+except ImportError:
+    print("Torch или ONNXRuntime не найдены. Будет установка в libs/ ...")
+    subprocess.check_call([
+        sys.executable,
+        "-m", "pip",
+        "install",
+        "--upgrade",
+        "--target", libs_path,
+        "torch",
+        "onnxruntime-gpu==1.21.0",
+        "--index-url", "https://download.pytorch.org/whl/cu128"
+    ])
+    import torch
+    import onnxruntime
+
+import tkinter_fix
 from modules import core
 
 if __name__ == '__main__':
